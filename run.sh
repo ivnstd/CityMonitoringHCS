10660ed07e4d8cac129274f49bb29139ec1ab772
ivnstd <nasty-iv@bk.ru>
2024-01-26 00:45:11 +0400

    add: structure for 2 microservices

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 
 source .venv/bin/activate
-uvicorn src.citymonitoringhcs.main:app --reload
+uvicorn src.nlp_service.app.main:app --host 0.0.0.0 --port 8001 --reload &
+uvicorn src.parser_service.app.main:app --host 0.0.0.0 --port 8002 --reload &
