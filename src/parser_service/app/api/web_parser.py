98bc2097c8bf1dcd5e79d46f7a9077e4fffedec1
ivnstd <nasty-iv@bk.ru>
2024-05-10 00:59:17 +0400

    fix: new site for web-parser

--- src/parser_service/app/api/web_parser.py
+++ src/parser_service/app/api/web_parser.py
@@ -7,11 +7,26 @@ from .scheme import Message
 from .logger import logger
 
 
-SITE_URL = 'https://www.kvs-saratov.ru'
-START_URL = 'https://www.kvs-saratov.ru/news/operativnyy-monitoring/'
-PAGE_URL = 'https://www.kvs-saratov.ru/news/operativnyy-monitoring/18443?PAGEN_1='
+URL = 'https://www.kvs-saratov.ru/news/operativnyy-monitoring/18443?PAGEN_1='
+URL2 = 'https://saratovvodokanal.ru/news/?PAGEN_1='
 MESSAGE_COUNT = 100
+LAST_ID = 32422     # локальный идентификатор последней записи на старом сайте
+ID_DELTA = 57       # разница локальных идентификаторов на сайтах
 
+SITE_ATTR = {
+    URL: {
+        'class': 'news_item',
+        'item': 'h3',
+        'date': 'span',
+        'date_padding': " 00:00:00"
+    },
+    URL2: {
+        'class': 'text_box',
+        'item': 'h4',
+        'date': 'time',
+        'date_padding': ":00"
+    }
+}
 
 async def fetch_html(url, client):
     """ Получение HTML-кода страницы по заданному URL """
@@ -29,23 +44,27 @@ async def get_page(url, client):
     return BeautifulSoup(html, 'html.parser')
 
 
-async def parse_page(url, last_message_id, client, is_new, count=MESSAGE_COUNT):
+async def parse_page(site_url, url, last_message_id, client, is_new, count=MESSAGE_COUNT):
     """ Парсинг одной страницы """
     page = await get_page(url, client)
-
-    news_items = page.find_all('div', {'class': 'news_item'})
+    site_attr = SITE_ATTR[site_url]
+    news_items = page.find_all('div', {'class': site_attr['class']})
     messages_info_list = []
 
     for item in news_items:
         try:
+            if len(item.attrs.get('class', [])) > 1:
+                continue
+
             id = int(item.find('a')['href'].split('/')[-2])
             # Если сообщение новое, парсим его
             if (not is_new and last_message_id - count <= id < last_message_id or
                     is_new and last_message_id < id <= last_message_id + count):
 
-                location = item.find('h3').get_text('', True)
-                date_str = item.find('span').get_text()
-                date = datetime.strptime(date_str + " 00:00:00", "%d.%m.%Y %H:%M:%S")
+                id = id + ID_DELTA * int(site_url == URL2)
+                location = item.find(site_attr['item']).get_text('', True)
+                date_str = item.find(site_attr['date']).get_text()
+                date = datetime.strptime(date_str + site_attr['date_padding'], "%d.%m.%Y %H:%M:%S")
                 messages_info_list.append(Message(
                     id=id,
                     source="kvs",
@@ -71,7 +90,7 @@ async def get_last_page_number(url, client):
 
 async def get_last_message_id(client):
     """ Определение идентификатора последнего сообщения на сайте """
-    page = await get_page(START_URL, client)
+    page = await get_page(URL, client)
 
     news_item = page.find('div', {'class': 'news_item'})
     last_message_id = int(news_item.find('a')['href'].split('/')[-2])
@@ -86,6 +105,14 @@ async def async_range(start, stop):
         await asyncio.sleep(0)
 
 
+def new_site_fix(last_message_id):
+    url = URL
+    if last_message_id >= LAST_ID:
+        url = URL2
+        last_message_id -= ID_DELTA
+    return url, last_message_id
+
+
 async def get_messages(last_message_id, is_new):
     async with httpx.AsyncClient() as client:
         if last_message_id == 0:
@@ -96,14 +123,16 @@ async def get_messages(last_message_id, is_new):
             if is_new:
                 last_message_id -= MESSAGE_COUNT
 
-        max_page_number = await get_last_page_number(START_URL, client)
+        url, last_message_id = new_site_fix(last_message_id)
+
+        max_page_number = await get_last_page_number(url, client)
         messages = []
 
         async for page_number in async_range(1, max_page_number + 1):
             messages_count = len(messages)
 
-            url = f'{PAGE_URL}{page_number}'
-            messages_info_list = await parse_page(url, last_message_id, client, is_new)
+            page_url = f'{url}{page_number}'
+            messages_info_list = await parse_page(url, page_url, last_message_id, client, is_new)
             messages.extend(messages_info_list)
 
             if messages_count == len(messages) and messages_count > 0:
